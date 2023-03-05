424e9ce0fbd40be4b8081ec21e26832d8b342775
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-03-05 15:55:53 +0100

    Cooking

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 700) {
-          $(".header-nav").css("background" , "#24252A");
+          $(".nav-links-a").css("color" , "#24252A");
         }
   
         else{
-            $(".header-nav").css("background" , "transparent");  	
+            $(".nav-links-a").css("color" , "#fff");  	
         }
     })
   })
